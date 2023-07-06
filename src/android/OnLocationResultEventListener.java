33a9cc518ef652a712e5592001b6bce06f353ba4
pcamilojunior <122461054+pcamilojunior@users.noreply.github.com>
2023-07-06 15:41:27 +0200

    Added huawei support (#19)
    * Added huawei support to Huawei Devices to getLocation and addWatch functionalities
    
    * Fix comments in PR, removed duplication of codes gms/hms, renaming variables, removed unsued code and fix to display dialog if device doesn't have goole neither huawei services installed
    
    * Fix comments in PR, removed duplication callback in Geolocaition.java
    
    ---------
    
    Co-authored-by: Paulo César <paulocesar@Paulos-Laptop.local>

--- src/android/OnLocationResultEventListener.java
+++ src/android/OnLocationResultEventListener.java
@@ -1,10 +1,11 @@
 package org.apache.cordova.geolocation;
 
-import com.google.android.gms.location.LocationResult;
+import android.location.Location;
+
+
+import java.util.List;
 
 interface OnLocationResultEventListener {
-
-    void onLocationResultSuccess(LocationContext locationContext, LocationResult result);
+    void onLocationResultSuccess(LocationContext locationContext, List<Location> locations);
     void onLocationResultError(LocationContext locationContext, LocationError error);
-
 }
